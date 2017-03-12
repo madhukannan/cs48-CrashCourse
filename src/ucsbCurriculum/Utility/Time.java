e0830c4e9ae484f409f10b45764e39d29e4afc9f
madhukannan <mkannan@umail.ucsb.edu>
2017-03-11 17:37:46 -0800

    Time now has startHour and endHour

--- src/ucsbCurriculum/Utility/Time.java
+++ src/ucsbCurriculum/Utility/Time.java
@@ -3,10 +3,20 @@ package ucsbCurriculum.Utility;
 public class Time {
     public int startTime;
     public int endTime;
+    public int startHour;
+    public int endHour;
     
     public Time(int start, int end) {
         startTime = start;
         endTime = end;
+        
+        int day = start/(24);
+        start = start - (day*24*60);
+        startHour = start/60;
+        
+        end = end - (day*60*24);
+        endHour = end/60;
+        
     }
     
     public int getStart() {
@@ -15,5 +25,13 @@ public class Time {
     
     public int getEnd() {
         return endTime;
+    }
+    
+    public int getStartHour(){
+    	return startHour;
+    }
+    
+    public int getEndHour(){
+    	return endHour;
     }
 }
